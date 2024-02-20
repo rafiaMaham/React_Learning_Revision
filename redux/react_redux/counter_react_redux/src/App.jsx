57bc3b5c5e6ac17443f4baf0e1cd537b14e00970
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-02-20 10:01:08 +0530

    commit

--- redux/react_redux/counter_react_redux/src/App.jsx
+++ redux/react_redux/counter_react_redux/src/App.jsx
@@ -1,17 +1,22 @@
+import { useSelector } from "react-redux";
 import Container from "./components/Container";
 import Controls from "./components/Controls";
 import DisplayCounter from "./components/DisplayCounter";
 import Header from "./components/Header";
+import PrivacyMessage from "./components/PrivacyMessage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const privacy = useSelector((store) => store.privacy);
+
   return (
     <>
       <center className="px-4 py-5 my-5 text-center">
         <Container>
           <Header />
           <div className="col-lg-6 mx-auto">
-            <DisplayCounter />
+            {privacy ? <PrivacyMessage /> : <DisplayCounter />}
+
             <Controls />
           </div>
         </Container>
--- redux/react_redux/counter_react_redux/src/components/Controls.jsx
+++ redux/react_redux/counter_react_redux/src/components/Controls.jsx
@@ -1,17 +1,47 @@
-import React from "react";
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
+import "../App.css";
 
 const Controls = () => {
   const dispatch = useDispatch();
+
+  const inputElement = useRef();
 
   const handleIncrement = () => {
     dispatch({
       type: "INCREMENT",
     });
   };
+
   const handleDecrement = () => {
     dispatch({
       type: "DECREMENT",
+    });
+  };
+
+  const handleAdd = () => {
+    dispatch({
+      type: "ADD",
+      payload: {
+        num: inputElement.current.value,
+      },
+    });
+    inputElement.current.value = "";
+  };
+
+  const handleSubtract = () => {
+    dispatch({
+      type: "SUBTRACT",
+      payload: {
+        num: inputElement.current.value,
+      },
+    });
+    inputElement.current.value = "";
+  };
+
+  const handlePrivacyToggle = () => {
+    dispatch({
+      type: "PRIVACY_TOGGLE",
     });
   };
 
@@ -32,25 +62,33 @@ const Controls = () => {
         >
           -1
         </button>
-        {/* <button type="button" className="btn btn-secondary">
-          Secondary
-        </button> */}
+        <button
+          type="button"
+          className="btn btn-warning"
+          onClick={handlePrivacyToggle}
+        >
+          Privacy Toggle
+        </button>
+      </div>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+        <input
+          type="text"
+          placeholder="Enter number"
+          className="number-input"
+          ref={inputElement}
+        />
 
-        {/* <button type="button" className="btn btn-danger">
-          Danger
+        <button type="button" className="btn btn-info" onClick={handleAdd}>
+          Add
         </button>
-        <button type="button" className="btn btn-warning">
-          Warning
+
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={handleSubtract}
+        >
+          Subtract
         </button>
-        <button type="button" className="btn btn-info">
-          Info
-        </button>
-        <button type="button" className="btn btn-light">
-          Light
-        </button>
-        <button type="button" className="btn btn-dark">
-          Dark
-        </button> */}
       </div>
     </>
   );
